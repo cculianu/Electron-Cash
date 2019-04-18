89584904c5a24667ea0c07453ea75304af4960ed
Axel Gembe <derago@gmail.com>
2019-04-18 20:36:42 +0700

    Improved QR code reader

---
+++ gui/qt/qrreader/video_widget.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+#
+# Electron Cash - lightweight Bitcoin client
+# Copyright (C) 2019 Axel Gembe <derago@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from typing import List
+
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtGui import QPixmap, QPainter, QPaintEvent, QPen, QPainterPath, QColor
+from PyQt5.QtCore import QRectF, Qt
+
+from electroncash.qrreaders import QrCodeResult
+
+class QrReaderVideoWidget(QWidget):
+    """
+    Simple widget for drawing a pixmap
+    """
+
+    USE_BILINEAR_FILTER = True
+
+    def __init__(self, parent: QWidget = None):
+        super().__init__(parent)
+        self.pixmap = None
+
+        self.qr_outline_pen = QPen()
+        self.qr_outline_pen.setColor(Qt.red)
+        self.qr_outline_pen.setWidth(3)
+        self.qr_outline_pen.setStyle(Qt.DotLine)
+        #self.qr_outline_pen.setDashOffset(4)
+
+        self.text_pen = QPen()
+        self.text_pen.setColor(Qt.black)
+
+        self.bg_box_pen = QPen()
+        self.bg_box_pen.setColor(Qt.black)
+        self.bg_box_pen.setStyle(Qt.DotLine)
+        self.bg_box_fill = QColor(255, 255, 255, 192)
+
+    def paintEvent(self, event: QPaintEvent):
+        if not self.pixmap:
+            return
+        painter = QPainter(self)
+        if self.USE_BILINEAR_FILTER:
+            painter.setRenderHint(QPainter.SmoothPixmapTransform)
+        painter.drawPixmap(self.rect(), self.pixmap, self.pixmap.rect())
+
+    def setPixmap(self, pixmap: QPixmap):
+        self.pixmap = pixmap
+        self.update()
+
+    def setResults(self, results: List[QrCodeResult]):
+        self.results = results
+
+    def drawOverlay(self):
+        # Small helper for tuple to QPoint
+        def toqp(point):
+            return QPoint(point[0], point[1])
+
+        # Starting from here we care about AA
+        painter.setRenderHint(QPainter.Antialiasing)
+
+        for res in self.results:
+            painter.setPen(self.qr_outline_pen)
+
+            num_points = len(res.points)
+            for i in range(0, num_points):
+                i_n = i + 1
+
+                line_from = toqp(res.points[i])
+                line_from += self.crop.topLeft()
+
+                line_to = toqp(res.points[i_n] if i_n < num_points else res.points[0])
+                line_to += self.crop.topLeft()
+
+                # Put the offset into the world transform
+                # transform = painter.worldTransform()
+                # offset = self.crop.topLeft()
+                # transform = transform.translate(offset.x(), offset.y())
+                # painter.setWorldTransform(transform)
+
+                painter.drawLine(line_from, line_to)
+
+            # Draw the data
+            font_metrics = painter.fontMetrics()
+            data_metrics_x = font_metrics.horizontalAdvance(res.data)
+            data_metrics_y = font_metrics.capHeight()
+
+            center_pos = toqp(res.center)
+            center_pos += self.crop.topLeft()
+
+            text_offset = QPoint(-data_metrics_x / 2, data_metrics_y / 2)
+            center_pos += text_offset
+
+            bg_rect_padding = 5
+            bg_rect_pos = center_pos - QPoint(bg_rect_padding, data_metrics_y + bg_rect_padding)
+            bg_rect = QRect(bg_rect_pos, QSize(data_metrics_x + (bg_rect_padding * 2), data_metrics_y + (bg_rect_padding * 2)))
+            bg_rect_path = QPainterPath()
+            bg_rect_path.addRoundedRect(QRectF(bg_rect), 5.0, 5.0, Qt.RelativeSize)
+            painter.setPen(self.bg_box_pen)
+            painter.fillPath(bg_rect_path, self.bg_box_fill)
+            painter.drawPath(bg_rect_path)
+
+            painter.setPen(self.text_pen)
+            painter.drawText(center_pos, res.data)
---
+++ gui/qt/utils/aspect_layout.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+#
+# Electron Cash - lightweight Bitcoin client
+# Copyright (C) 2019 Axel Gembe <derago@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from PyQt5.QtWidgets import QLayout, QWidget, QLayoutItem
+from PyQt5.QtCore import Qt, QSize, QRect, QPoint
+
+class FixedAspectRatioLayout(QLayout):
+    def __init__(self, parent: QWidget = None, aspect_ratio: float = 1.0):
+        super().__init__(parent)
+        self.aspect_ratio = aspect_ratio
+        self.item = None
+
+    def setAspectRatio(self, aspect_ratio: float = 1.0):
+        self.aspect_ratio = aspect_ratio
+        self.update()
+
+    def addItem(self, item: QLayoutItem):
+        if self.item:
+            raise RuntimeError('FixedAspectRatioLayout may only contain a single item')
+        self.item = item
+
+    def count(self) -> int:
+        return 1 if self.item else 0
+
+    def itemAt(self, index: int) -> QLayoutItem:
+        return self.item if index == 0 else None
+
+    def takeAt(self, index: int) -> QLayoutItem:
+        res = self.itemAt(index)
+        if index == 0:
+            self.item = None
+        return res
+
+    def _getContentsMarginsSize(self) -> QSize:
+        margins = self.getContentsMargins()
+        return QSize(margins[0] + margins[2], margins[1] + margins[3])
+
+    def setGeometry(self, rect: QRect):
+        super().setGeometry(rect)
+        if not self.item:
+            return
+
+        content_size = rect.size() - self._getContentsMarginsSize()
+        content_aspect_ratio = content_size.width() / content_size.height()
+        item_rect = QRect(QPoint(0, 0), QSize(
+            content_size.width() if content_aspect_ratio < self.aspect_ratio else content_size.height() * self.aspect_ratio,
+            content_size.height() if content_aspect_ratio > self.aspect_ratio else content_size.width() / self.aspect_ratio
+        ))
+
+        content_margins = self.getContentsMargins()
+        free_space = content_size - item_rect.size()
+
+        if free_space.width() > 0:
+            if self.item.alignment() & Qt.AlignLeft:
+                item_rect.moveLeft(content_margins[1])
+            elif self.item.alignment() & Qt.AlignRight:
+                item_rect.moveRight(content_size.width() + content_margins[3])
+            else:
+                item_rect.moveLeft(content_margins[1] + (free_space.width() / 2))
+
+        if free_space.height() > 0:
+            if self.item.alignment() & Qt.AlignTop:
+                item_rect.moveTop(content_margins[0])
+            elif self.item.alignment() & Qt.AlignBottom:
+                item_rect.moveBottom(content_size.height() + content_margins[2])
+            else:
+                item_rect.moveTop(content_margins[0] + (free_space.height() / 2))
+
+        self.item.widget().setGeometry(item_rect)
+
+    def sizeHint(self) -> QSize:
+        if not self.item:
+            return self._getContentsMarginsSize()
+        return self._getContentsMarginsSize() + self.item.sizeHint()
+
+    def minimumSize(self) -> QSize:
+        if not self.item:
+            return self._getContentsMarginsSize()
+        return self._getContentsMarginsSize() + self.item.minimumSize()
+
+    def expandingDirections(self) -> Qt.Orientations:
+        return Qt.Horizontal | Qt.Vertical
